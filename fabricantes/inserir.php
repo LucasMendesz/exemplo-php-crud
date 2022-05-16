7c3e0edc36d237a61fd926e5f5b2b7e778fdaf6d
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-05-16 11:49:25 -0300

    Formulário

---
+++ fabricantes/inserir.php
@@ -0,0 +1,35 @@
+<?php
+// Verificando se o botão do formulário foi acionado
+if( isset($_POST['inserir']) ) {
+    echo "ok!";
+}
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fabricantes - Inserir</title>
+</head>
+<body>
+
+<div class="container">
+    <h1>Fabricantes | INSERT</h1>
+    <hr>
+
+    <form action="" method="post">
+        <p>
+            <label for="text">Nome</label>
+            <input type="text" name="nome" id="nome">
+        </p>
+        <button type="submit" name="inserir">inserir fabricante</button>
+    </form>
+</div>
+    
+</body>
+</html>
