7f861bd2b19713ecb6501b66d4fa914bc59ea120
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-06-29 09:51:21 -0300

    Delete Funções fabricante

--- src/Fabricante.php
+++ src/Fabricante.php
@@ -93,6 +93,19 @@ public function atualizarFabricante():void {
     };
   }
 
+public function excluirFabricante():void {
+  $sql = "DELETE FROM fabricantes WHERE id = :id";
+ 
+  try {
+      $consulta = $this->conexao->prepare($sql);
+ 
+      $consulta->bindParam(':id', $this->id, PDO::PARAM_INT);
+      $consulta->execute();
+  } catch (Exception $erro) {
+      die("Erro: " .$erro->getMessage() );
+  }
+ }
+
   public function getId(): int
   {
     return $this->id;
