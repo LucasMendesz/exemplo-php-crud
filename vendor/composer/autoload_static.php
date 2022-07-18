785a3d739b4f20c34cb887488111f085bc0483cb
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-07-18 11:42:17 -0300

    Instalando PHP/MAILER

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -12,6 +12,10 @@ class ComposerStaticInitc1f39d425b5e7401aebe94dc49f08b44
             'Svg\\' => 4,
             'Sabberworm\\CSS\\' => 15,
         ),
+        'P' => 
+        array (
+            'PHPMailer\\PHPMailer\\' => 20,
+        ),
         'M' => 
         array (
             'Masterminds\\' => 12,
@@ -39,6 +43,10 @@ class ComposerStaticInitc1f39d425b5e7401aebe94dc49f08b44
         'Sabberworm\\CSS\\' => 
         array (
             0 => __DIR__ . '/..' . '/sabberworm/php-css-parser/src',
+        ),
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
         ),
         'Masterminds\\' => 
         array (
